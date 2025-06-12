ba98de3a0af60e4241be8ade6a00f0972c6963e6
mah10111 <hodaei.mahboobeh1373@gmail.com>
2025-06-12 12:18:41 +0330

    Update utils.py

--- hanna_pytorch/utils.py
+++ hanna_pytorch/utils.py
@@ -10,7 +10,6 @@ from datetime import datetime
 import shutil
 import math
 import tensorflow as tf
-from tensorboardX import SummaryWriter
 
 import torch
 from torch.optim.lr_scheduler import _LRScheduler
@@ -157,51 +156,23 @@ def _set_file(path):
     hdl.setFormatter(_MyFormatter(datefmt='%m%d %H:%M:%S'))
     _logger.addHandler(hdl)
 
+from torch.utils.tensorboard import SummaryWriter
 
 class Tensorboard:
     def __init__(self, logdir):
-        #self.writer = tf.summary.FileWriter(logdir)
-        self.writer = tf.summary.create_file_writer(logdir)
+        self.writer = SummaryWriter(logdir)
 
     def close(self):
         self.writer.close()
 
-    #def log_scalar(self, tag, value, global_step):
-    #    summary = tf.Summary()
-    #    summary.value.add(tag=tag, simple_value=value)
-    #    self.writer.add_summary(summary, global_step=global_step)
-    #   self.writer.flush()
-    def log_scalar(self,tag, value, global_step):
-        self.writer.add_scalar(tag, global_step, value)
-        self.writer = SummaryWriter(logdir)
+    def log_scalar(self, tag, value, global_step):
+        self.writer.add_scalar(tag, value, global_step)
+
     def log_image(self, tag, img, global_step):
-        s = io.BytesIO()
-        Image.fromarray(img).save(s, format='png')
-
-        img_summary = tf.Summary.Image(encoded_image_string=s.getvalue(),
-                                   height=img.shape[0],
-                                   width=img.shape[1])
-
-        summary = tf.Summary()
-        summary.value.add(tag=tag, image=img_summary)
-        self.writer.add_summary(summary, global_step=global_step)
-        self.writer.flush()
+        self.writer.add_image(tag, img, global_step, dataformats='HWC')
 
     def log_plot(self, tag, figure, global_step):
-        plot_buf = io.BytesIO()
-        figure.savefig(plot_buf, format='png')
-        plot_buf.seek(0)
-        img = Image.open(plot_buf)
-        img_ar = np.array(img)
-
-        img_summary = tf.Summary.Image(encoded_image_string=plot_buf.getvalue(),
-                           height=img_ar.shape[0],
-                           width=img_ar.shape[1])
-
-        summary = tf.Summary()
-        summary.value.add(tag=tag, image=img_summary)
-        self.writer.add_summary(summary, global_step=global_step)
-        self.writer.flush()
+        self.writer.add_figure(tag, figure, global_step)
 
 
 
