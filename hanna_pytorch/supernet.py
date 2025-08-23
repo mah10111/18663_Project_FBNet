ff3852037a64e1a3562d561bd47a7507cef89bdd
mah10111 <hodaei.mahboobeh1373@gmail.com>
2025-08-23 12:22:23 +0330

    supernet.py

--- hanna_pytorch/supernet.py
+++ hanna_pytorch/supernet.py
@@ -11,7 +11,8 @@ np.random.seed(0)
 sns.set()
 from utils import AvgrageMeter, weights_init, \
                   CosineDecayLR, Tensorboard
-from data_parallel import DataParallel
+#from data_parallel import DataParallel
+from torch.nn import DataParallel
 
 class MixedOp(nn.Module):
   """Mixed operation.
