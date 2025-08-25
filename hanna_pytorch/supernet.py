b1bf26125233138593a7251bfb0bca4b40579482
mah10111 <hodaei.mahboobeh1373@gmail.com>
2025-08-25 12:02:53 +0330

    Update supernet.py

--- hanna_pytorch/supernet.py
+++ hanna_pytorch/supernet.py
@@ -12,26 +12,27 @@ import numpy as np
 import seaborn as sns
 import matplotlib.pyplot as plt
 
-
 from utils import Tensorboard, weights_init, load_flops_lut, AvgrageMeter, load_flops_lut, CosineDecayLR
-class MixedOp(nn.Module):
-  """Mixed operation.
-  Weighted sum of blocks.
-  """
-  def __init__(self, blocks):
-    super(MixedOp, self).__init__()
-    self._ops = nn.ModuleList()
-    for op in blocks:
-      self._ops.append(op)
 
-  def forward(self, x, weights):
-    tmp = []
-    for i, op in enumerate(self._ops):
-      r = op(x)
-      w = weights[..., i].reshape((-1, 1, 1, 1))
-      res = w * r
-      tmp.append(res)
-    return sum(tmp)
+
+class MixedOp(nn.Module):
+    """Mixed operation.
+    Weighted sum of blocks.
+    """
+    def __init__(self, blocks):
+        super(MixedOp, self).__init__()
+        self._ops = nn.ModuleList()
+        for op in blocks:
+            self._ops.append(op)
+
+    def forward(self, x, weights):
+        tmp = []
+        for i, op in enumerate(self._ops):
+            r = op(x)
+            w = weights[..., i].reshape((-1, 1, 1, 1))
+            res = w * r
+            tmp.append(res)
+        return sum(tmp)
 
 
 class FBNet(nn.Module):
@@ -44,7 +45,7 @@ class FBNet(nn.Module):
                  beta=0,
                  gamma=0,
                  delta=0,
-				 eta=0,
+                 eta=0,
                  criterion=nn.CrossEntropyLoss(),
                  dim_feature=1984):
         super(FBNet, self).__init__()
@@ -78,7 +79,7 @@ class FBNet(nn.Module):
         for b in blocks:
             if isinstance(b, list):
                 num_block = len(b)
-                theta = nn.Parameter(torch.ones((num_block, )).cuda(), requires_grad=True)
+                theta = nn.Parameter(torch.ones((num_block,)).cuda(), requires_grad=True)
                 init_func(theta)
                 self.theta.append(theta)
                 self._ops.append(MixedOp(b))
@@ -105,14 +106,14 @@ class FBNet(nn.Module):
 
         # نرمال‌سازی سرعت
         max_len = max([len(s) for s in self._speed])
-        iden_s = sum(s[max_len-1] for s in self._speed if len(s) == max_len) / sum(1 for s in self._speed if len(s) == max_len)
+        iden_s = sum(s[max_len - 1] for s in self._speed if len(s) == max_len) / sum(1 for s in self._speed if len(s) == max_len)
         for i in range(len(self._speed)):
             if len(self._speed[i]) == max_len - 1:
                 self._speed[i].append(iden_s)
 
         # نرمال‌سازی انرژی
         max_len = max([len(s) for s in self._energy])
-        iden_s = sum(s[max_len-1] for s in self._energy if len(s) == max_len) / sum(1 for s in self._energy if len(s) == max_len)
+        iden_s = sum(s[max_len - 1] for s in self._energy if len(s) == max_len) / sum(1 for s in self._energy if len(s) == max_len)
         for i in range(len(self._energy)):
             if len(self._energy[i]) == max_len - 1:
                 self._energy[i].append(iden_s)
@@ -128,279 +129,90 @@ class FBNet(nn.Module):
         self.classifier = nn.Linear(dim_feature, num_classes)
 
     def forward(self, input, target, temperature=5.0, theta_list=None):
-     self.rounds_per_layer = []
+        self.rounds_per_layer = []
 
-     batch_size = input.size()[0]
-     self.batch_size = batch_size
-     data = self._input_conv(input)
-     theta_idx = 0
-     lat = []
-     ener = []
-     flops_acc = []  # 🔵 برای محاسبه FLOPs
+        batch_size = input.size()[0]
+        self.batch_size = batch_size
+        data = self._input_conv(input)
+        theta_idx = 0
+        lat = []
+        ener = []
+        flops_acc = []  # 🔵 برای محاسبه FLOPs
 
-     for l_idx in range(self._input_conv_count, len(self._blocks)):
-         block = self._blocks[l_idx]
-         if isinstance(block, list):
-             blk_len = len(block)
+        for l_idx in range(self._input_conv_count, len(self._blocks)):
+            block = self._blocks[l_idx]
+            if isinstance(block, list):
+                blk_len = len(block)
 
-             if theta_list is None:
-                 theta = self.theta[theta_idx]
-             else:
-                 theta = theta_list[theta_idx]
- 
-             t = theta.repeat(batch_size, 1)
-             weight = nn.functional.gumbel_softmax(t, temperature)
- 
-            # --- FLOPs ---
-             if self._flops is not None:
-                 flops = self._flops[theta_idx][:blk_len].to(weight.device)
-                 flops_ = weight * flops.repeat(batch_size, 1)
-                 flops_acc.append(torch.sum(flops_))
- 
-            # --- Latency & Energy ---
-             speed = self._speed[theta_idx][:blk_len].to(weight.device)
-             energy = self._energy[theta_idx][:blk_len].to(weight.device)
-             lat_ = weight * speed.repeat(batch_size, 1)
-             ener_ = weight * energy.repeat(batch_size, 1)
-             lat.append(torch.sum(lat_))
-             ener.append(torch.sum(ener_))
-			# --- Hardware Rounds ---
-# تعداد عملیات این لایه (تقریباً با FLOPs یکیه)
-# flops_ بر حسب GigaOps است → باید به Ops تبدیل کنیم
-			 ops_this_layer = torch.sum(flops_).item() * 1e9  
+                if theta_list is None:
+                    theta = self.theta[theta_idx]
+                else:
+                    theta = theta_list[theta_idx]
 
-# ظرفیت هر PE
-			 pe_capacity = 50000  
-			 num_pe = 20
-			 total_capacity = num_pe * pe_capacity
+                t = theta.repeat(batch_size, 1)
+                weight = nn.functional.gumbel_softmax(t, temperature)
 
-# چند دور طول می‌کشد تا این لایه روی سخت‌افزار اجرا شود
-			 rounds = int((ops_this_layer + total_capacity - 1) // total_capacity)
-			 self.rounds_per_layer.append(rounds)
+                # --- FLOPs ---
+                if self._flops is not None:
+                    flops = self._flops[theta_idx][:blk_len].to(weight.device)
+                    flops_ = weight * flops.repeat(batch_size, 1)
+                    flops_acc.append(torch.sum(flops_))
 
-             data = self._ops[theta_idx](data, weight)
-             theta_idx += 1
+                # --- Latency & Energy ---
+                speed = self._speed[theta_idx][:blk_len].to(weight.device)
+                energy = self._energy[theta_idx][:blk_len].to(weight.device)
+                lat_ = weight * speed.repeat(batch_size, 1)
+                ener_ = weight * energy.repeat(batch_size, 1)
+                lat.append(torch.sum(lat_))
+                ener.append(torch.sum(ener_))
 
-         else:
-             break
+                # --- Hardware Rounds ---
+                # flops_ بر حسب GigaOps است → باید به Ops تبدیل کنیم
+                ops_this_layer = torch.sum(flops_).item() * 1e9
 
-    # --- خروجی نهایی ---
-     data = self._output_conv(data)
-     lat = sum(lat)
-     ener = sum(ener)
-     data = nn.functional.avg_pool2d(data, data.size()[2:])
-     data = data.reshape((batch_size, -1))
-     logits = self.classifier(data)
+                # ظرفیت هر PE
+                pe_capacity = 50000
+                num_pe = 20
+                total_capacity = num_pe * pe_capacity
 
-     self.ce = self._criterion(logits, target).sum()
-     self.lat_loss = lat / batch_size
-     self.ener_loss = ener / batch_size
-     self.loss = self.ce + self._alpha * self.lat_loss.pow(self._beta) + \
-                 self._gamma * self.ener_loss.pow(self._delta)
+                # چند دور طول می‌کشد تا این لایه روی سخت‌افزار اجرا شود
+                rounds = int((ops_this_layer + total_capacity - 1) // total_capacity)
+                self.rounds_per_layer.append(rounds)
 
-     self.flops_loss = sum(flops_acc) / batch_size if len(flops_acc) > 0 \
-                       else torch.tensor(0.0, device=input.device)  # 🔵 FLOPs
+                data = self._ops[theta_idx](data, weight)
+                theta_idx += 1
+            else:
+                break
 
-     pred = torch.argmax(logits, dim=1)
-     self.acc = torch.sum(pred == target).float() / batch_size
+        # --- خروجی نهایی ---
+        data = self._output_conv(data)
+        lat = sum(lat)
+        ener = sum(ener)
+        data = nn.functional.avg_pool2d(data, data.size()[2:])
+        data = data.reshape((batch_size, -1))
+        logits = self.classifier(data)
 
-    # --- در انتهای forward ---
-    # max_rounds = max(self.rounds_per_layer) if len(self.rounds_per_layer) > 0 else 0
-     self.max_rounds = max(self.rounds_per_layer) if len(self.rounds_per_layer) > 0 else 0
+        self.ce = self._criterion(logits, target).sum()
+        self.lat_loss = lat / batch_size
+        self.ener_loss = ener / batch_size
+        self.loss = self.ce + self._alpha * self.lat_loss.pow(self._beta) + \
+                    self._gamma * self.ener_loss.pow(self._delta)
 
-# اضافه کردن penalty برای max_rounds
-     rounds_loss = torch.tensor(self.max_rounds, dtype=torch.float32, device=input.device)
+        self.flops_loss = sum(flops_acc) / batch_size if len(flops_acc) > 0 \
+            else torch.tensor(0.0, device=input.device)  # 🔵 FLOPs
 
-     self.loss = ( self.ce
-         + self._alpha * self.lat_loss.pow(self._beta)
-         + self._gamma * self.ener_loss.pow(self._delta)
-         + self._eta * rounds_loss  # 🔵 پارامتر جدید برای کنترل اهمیت max_rounds
-                  )
-     return self.loss, self.ce, self.lat_loss, self.acc, self.ener_loss
+        pred = torch.argmax(logits, dim=1)
+        self.acc = torch.sum(pred == target).float() / batch_size
 
-class Trainer(object):
-  """Training network parameters and theta separately.
-  """
-  def __init__(self, network,
-               w_lr=0.01,
-               w_mom=0.9,
-               w_wd=1e-4,
-               t_lr=0.001,
-               t_wd=3e-3,
-               t_beta=(0.5, 0.999),
-               init_temperature=5.0,
-               temperature_decay=0.965,
-               logger=logging,
-               lr_scheduler={'T_max' : 200},
-               gpus=[0],
-               save_theta_prefix='',
-	       save_tb_log=''):
-    assert isinstance(network, FBNet)
-    network.apply(weights_init)
-    network = network.train().cuda()
-    if isinstance(gpus, str):
-      gpus = [int(i) for i in gpus.strip().split(',')]
-    network = DataParallel(network, gpus)
-    self.gpus = gpus
-    self._mod = network
-    theta_params = network.module.theta
-    mod_params = network.parameters()
-    self.theta = theta_params
-    self.w = mod_params
-    self._tem_decay = temperature_decay
-    self.temp = init_temperature
-    self.logger = logger
-    #self.tensorboard = Tensorboard('logs/'+save_tb_log)
-    self.tensorboard = Tensorboard('logs/' + (save_tb_log if save_tb_log is not None else 'default_log'))
+        # --- در انتهای forward ---
+        self.max_rounds = max(self.rounds_per_layer) if len(self.rounds_per_layer) > 0 else 0
 
-    self.save_theta_prefix = save_theta_prefix
+        # اضافه کردن penalty برای max_rounds
+        rounds_loss = torch.tensor(self.max_rounds, dtype=torch.float32, device=input.device)
 
-    self._acc_avg = AvgrageMeter('acc')
-    self._ce_avg = AvgrageMeter('ce')
-    self._lat_avg = AvgrageMeter('lat')
-    self._loss_avg = AvgrageMeter('loss')
-    self._ener_avg = AvgrageMeter('ener')
-
-    self.w_opt = torch.optim.SGD(
-                    mod_params,
-                    w_lr,
-                    momentum=w_mom,
-                    weight_decay=w_wd)
-    
-    self.w_sche = CosineDecayLR(self.w_opt, **lr_scheduler)
-
-    self.t_opt = torch.optim.Adam(
-                    theta_params,
-                    lr=t_lr, betas=t_beta,
-                    weight_decay=t_wd)
-
-  def train_w(self, input, target, decay_temperature=False):
-    """Update model parameters.
-    """
-    self.w_opt.zero_grad()
-    loss, ce, lat, acc,ener = self._mod(input, target, self.temp)
-    loss.backward()
-    self.w_opt.step()
-    if decay_temperature:
-      tmp = self.temp
-      self.temp *= self._tem_decay
-      self.logger.info("Change temperature from %.5f to %.5f" % (tmp, self.temp))
-    return loss.item(), ce.item(), lat.item(), acc.item(),ener.item()
-  
-  def train_t(self, input, target, decay_temperature=False):
-    """Update theta.
-    """
-    self.t_opt.zero_grad()
-    loss, ce, lat, acc,ener = self._mod(input, target, self.temp)
-    loss.backward()
-    self.t_opt.step()
-    if decay_temperature:
-      tmp = self.temp
-      self.temp *= self._tem_decay
-      self.logger.info("Change temperature from %.5f to %.5f" % (tmp, self.temp))
-    return loss.item(), ce.item(), lat.item(), acc.item(),ener.item()
-  
-  def decay_temperature(self, decay_ratio=None):
-    tmp = self.temp
-    if decay_ratio is None:
-      self.temp *= self._tem_decay
-    else:
-      self.temp *= decay_ratio
-    self.logger.info("Change temperature from %.5f to %.5f" % (tmp, self.temp))
-  
-  def _step(self, input, target, 
-            epoch, step,
-            log_frequence,
-            func):
-    """Perform one step of training.
-    """
-    input = input.cuda()
-    target = target.cuda()
-    loss, ce, lat, acc ,ener= func(input, target)
-
-    # Get status
-    #batch_size = self.module._mod.batch_size
-    try:
-     batch_size = self._mod.module.batch_size
-    except AttributeError:
-     batch_size = self._mod.batch_size
-    self._acc_avg.update(acc)
-    self._ce_avg.update(ce)
-    self._lat_avg.update(lat)
-    self._loss_avg.update(loss)
-    self._ener_avg.update(ener)
-
-    if step > 1 and (step % log_frequence == 0):
-      self.toc = time.time()
-      speed = 1.0 * (batch_size * log_frequence) / (self.toc - self.tic)
-      self.tensorboard.log_scalar('Total Loss', self._loss_avg.getValue(), step)
-      self.tensorboard.log_scalar('Accuracy',self._acc_avg.getValue(),step)
-      self.tensorboard.log_scalar('Latency',self._lat_avg.getValue(),step)
-      self.tensorboard.log_scalar('Energy',self._ener_avg.getValue(),step)
-      self.logger.info("Epoch[%d] Batch[%d] Speed: %.6f samples/sec %s %s %s %s %s" 
-              % (epoch, step, speed, self._loss_avg, 
-                 self._acc_avg, self._ce_avg, self._lat_avg,self._ener_avg))
-      map(lambda avg: avg.reset(), [self._loss_avg, self._acc_avg, 
-                                    self._ce_avg, self._lat_avg,self._ener_avg])
-      self.tic = time.time()
-  
-  def search(self, train_w_ds,
-            train_t_ds,
-            total_epoch=90,
-            start_w_epoch=10,
-            log_frequence=100):
-    """Search model.
-    """
-    assert start_w_epoch >= 1, "Start to train w"
-    self.tic = time.time()
-    for epoch in range(start_w_epoch):
-      self.logger.info("Start to train w for epoch %d" % epoch)
-      for step, (input, target) in enumerate(train_w_ds):
-        self._step(input, target, epoch, 
-                   step, log_frequence,
-                   lambda x, y: self.train_w(x, y, False))
-        self.w_sche.step()
-        self.tensorboard.log_scalar('Learning rate curve',self.w_sche.last_epoch,self.w_opt.param_groups[0]['lr'])
-        #print(self.w_sche.last_epoch, self.w_opt.param_groups[0]['lr'])
-
-    self.tic = time.time()
-    for epoch in range(total_epoch):
-      self.logger.info("Start to train theta for epoch %d" % (epoch+start_w_epoch))
-      for step, (input, target) in enumerate(train_t_ds):
-        self._step(input, target, epoch + start_w_epoch, 
-                   step, log_frequence,
-                   lambda x, y: self.train_t(x, y, False))
-        self.save_theta('./theta-result/%s_theta_epoch_%d.txt' % 
-                    (self.save_theta_prefix, epoch+start_w_epoch), epoch)
-      self.decay_temperature()
-      self.logger.info("Start to train w for epoch %d" % (epoch+start_w_epoch))
-      for step, (input, target) in enumerate(train_w_ds):
-        self._step(input, target, epoch + start_w_epoch, 
-                   step, log_frequence,
-                   lambda x, y: self.train_w(x, y, False))
-        self.w_sche.step()
-      self.tensorboard.close()
-
-  def save_theta(self, save_path='theta.txt', epoch=0):
-    # ایجاد پوشه در صورت عدم وجود
-    os.makedirs(os.path.dirname(save_path), exist_ok=True)
-
-    res = []
-    with open(save_path, 'w') as f:
-        for i, t in enumerate(self.theta):
-            t_list = list(t.detach().cpu().numpy())
-            if len(t_list) < 9:
-                t_list.append(0.00)
-            max_index = t_list.index(max(t_list))
-            self.tensorboard.log_scalar('Layer %s' % str(i), max_index + 1, epoch)
-            res.append(t_list)
-            s = ' '.join([str(tmp) for tmp in t_list])
-            f.write(s + '\n')
-
-        val = np.array(res)
-        ax = sns.heatmap(val, cbar=True, annot=True)
-        ax.figure.savefig(save_path[:-3] + 'png')
-        # self.tensorboard.log_image('Theta Values', val, epoch)
-        plt.close()
-
-    return res
+        self.loss = (self.ce
+                     + self._alpha * self.lat_loss.pow(self._beta)
+                     + self._gamma * self.ener_loss.pow(self._delta)
+                     + self._eta * rounds_loss  # 🔵 پارامتر جدید برای کنترل اهمیت max_rounds
+                     )
+        return self.loss, self.ce, self.lat_loss, self.acc, self.ener_loss
