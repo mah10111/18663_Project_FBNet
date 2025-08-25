7cdc5fb900c7ebb4ec7d09fa162a38dc167bfeaf
mah10111 <hodaei.mahboobeh1373@gmail.com>
2025-08-25 12:04:55 +0330

    Update supernet.py

--- hanna_pytorch/supernet.py
+++ hanna_pytorch/supernet.py
@@ -216,3 +216,189 @@ class FBNet(nn.Module):
                      + self._eta * rounds_loss  # 🔵 پارامتر جدید برای کنترل اهمیت max_rounds
                      )
         return self.loss, self.ce, self.lat_loss, self.acc, self.ener_loss
+class Trainer(object):
+  """Training network parameters and theta separately.
+  """
+  def __init__(self, network,
+               w_lr=0.01,
+               w_mom=0.9,
+               w_wd=1e-4,
+               t_lr=0.001,
+               t_wd=3e-3,
+               t_beta=(0.5, 0.999),
+               init_temperature=5.0,
+               temperature_decay=0.965,
+               logger=logging,
+               lr_scheduler={'T_max' : 200},
+               gpus=[0],
+               save_theta_prefix='',
+	       save_tb_log=''):
+    assert isinstance(network, FBNet)
+    network.apply(weights_init)
+    network = network.train().cuda()
+    if isinstance(gpus, str):
+      gpus = [int(i) for i in gpus.strip().split(',')]
+    network = DataParallel(network, gpus)
+    self.gpus = gpus
+    self._mod = network
+    theta_params = network.module.theta
+    mod_params = network.parameters()
+    self.theta = theta_params
+    self.w = mod_params
+    self._tem_decay = temperature_decay
+    self.temp = init_temperature
+    self.logger = logger
+    #self.tensorboard = Tensorboard('logs/'+save_tb_log)
+    self.tensorboard = Tensorboard('logs/' + (save_tb_log if save_tb_log is not None else 'default_log'))
+
+    self.save_theta_prefix = save_theta_prefix
+
+    self._acc_avg = AvgrageMeter('acc')
+    self._ce_avg = AvgrageMeter('ce')
+    self._lat_avg = AvgrageMeter('lat')
+    self._loss_avg = AvgrageMeter('loss')
+    self._ener_avg = AvgrageMeter('ener')
+
+    self.w_opt = torch.optim.SGD(
+                    mod_params,
+                    w_lr,
+                    momentum=w_mom,
+                    weight_decay=w_wd)
+    
+    self.w_sche = CosineDecayLR(self.w_opt, **lr_scheduler)
+
+    self.t_opt = torch.optim.Adam(
+                    theta_params,
+                    lr=t_lr, betas=t_beta,
+                    weight_decay=t_wd)
+
+  def train_w(self, input, target, decay_temperature=False):
+    """Update model parameters.
+    """
+    self.w_opt.zero_grad()
+    loss, ce, lat, acc,ener = self._mod(input, target, self.temp)
+    loss.backward()
+    self.w_opt.step()
+    if decay_temperature:
+      tmp = self.temp
+      self.temp *= self._tem_decay
+      self.logger.info("Change temperature from %.5f to %.5f" % (tmp, self.temp))
+    return loss.item(), ce.item(), lat.item(), acc.item(),ener.item()
+  
+  def train_t(self, input, target, decay_temperature=False):
+    """Update theta.
+    """
+    self.t_opt.zero_grad()
+    loss, ce, lat, acc,ener = self._mod(input, target, self.temp)
+    loss.backward()
+    self.t_opt.step()
+    if decay_temperature:
+      tmp = self.temp
+      self.temp *= self._tem_decay
+      self.logger.info("Change temperature from %.5f to %.5f" % (tmp, self.temp))
+    return loss.item(), ce.item(), lat.item(), acc.item(),ener.item()
+  
+  def decay_temperature(self, decay_ratio=None):
+    tmp = self.temp
+    if decay_ratio is None:
+      self.temp *= self._tem_decay
+    else:
+      self.temp *= decay_ratio
+    self.logger.info("Change temperature from %.5f to %.5f" % (tmp, self.temp))
+  
+  def _step(self, input, target, 
+            epoch, step,
+            log_frequence,
+            func):
+    """Perform one step of training.
+    """
+    input = input.cuda()
+    target = target.cuda()
+    loss, ce, lat, acc ,ener= func(input, target)
+
+    # Get status
+    #batch_size = self.module._mod.batch_size
+    try:
+     batch_size = self._mod.module.batch_size
+    except AttributeError:
+     batch_size = self._mod.batch_size
+    self._acc_avg.update(acc)
+    self._ce_avg.update(ce)
+    self._lat_avg.update(lat)
+    self._loss_avg.update(loss)
+    self._ener_avg.update(ener)
+
+    if step > 1 and (step % log_frequence == 0):
+      self.toc = time.time()
+      speed = 1.0 * (batch_size * log_frequence) / (self.toc - self.tic)
+      self.tensorboard.log_scalar('Total Loss', self._loss_avg.getValue(), step)
+      self.tensorboard.log_scalar('Accuracy',self._acc_avg.getValue(),step)
+      self.tensorboard.log_scalar('Latency',self._lat_avg.getValue(),step)
+      self.tensorboard.log_scalar('Energy',self._ener_avg.getValue(),step)
+      self.logger.info("Epoch[%d] Batch[%d] Speed: %.6f samples/sec %s %s %s %s %s" 
+              % (epoch, step, speed, self._loss_avg, 
+                 self._acc_avg, self._ce_avg, self._lat_avg,self._ener_avg))
+      map(lambda avg: avg.reset(), [self._loss_avg, self._acc_avg, 
+                                    self._ce_avg, self._lat_avg,self._ener_avg])
+      self.tic = time.time()
+  
+  def search(self, train_w_ds,
+            train_t_ds,
+            total_epoch=90,
+            start_w_epoch=10,
+            log_frequence=100):
+    """Search model.
+    """
+    assert start_w_epoch >= 1, "Start to train w"
+    self.tic = time.time()
+    for epoch in range(start_w_epoch):
+      self.logger.info("Start to train w for epoch %d" % epoch)
+      for step, (input, target) in enumerate(train_w_ds):
+        self._step(input, target, epoch, 
+                   step, log_frequence,
+                   lambda x, y: self.train_w(x, y, False))
+        self.w_sche.step()
+        self.tensorboard.log_scalar('Learning rate curve',self.w_sche.last_epoch,self.w_opt.param_groups[0]['lr'])
+        #print(self.w_sche.last_epoch, self.w_opt.param_groups[0]['lr'])
+
+    self.tic = time.time()
+    for epoch in range(total_epoch):
+      self.logger.info("Start to train theta for epoch %d" % (epoch+start_w_epoch))
+      for step, (input, target) in enumerate(train_t_ds):
+        self._step(input, target, epoch + start_w_epoch, 
+                   step, log_frequence,
+                   lambda x, y: self.train_t(x, y, False))
+        self.save_theta('./theta-result/%s_theta_epoch_%d.txt' % 
+                    (self.save_theta_prefix, epoch+start_w_epoch), epoch)
+      self.decay_temperature()
+      self.logger.info("Start to train w for epoch %d" % (epoch+start_w_epoch))
+      for step, (input, target) in enumerate(train_w_ds):
+        self._step(input, target, epoch + start_w_epoch, 
+                   step, log_frequence,
+                   lambda x, y: self.train_w(x, y, False))
+        self.w_sche.step()
+      self.tensorboard.close()
+
+  def save_theta(self, save_path='theta.txt', epoch=0):
+    # ایجاد پوشه در صورت عدم وجود
+    os.makedirs(os.path.dirname(save_path), exist_ok=True)
+
+    res = []
+    with open(save_path, 'w') as f:
+        for i, t in enumerate(self.theta):
+            t_list = list(t.detach().cpu().numpy())
+            if len(t_list) < 9:
+                t_list.append(0.00)
+            max_index = t_list.index(max(t_list))
+            self.tensorboard.log_scalar('Layer %s' % str(i), max_index + 1, epoch)
+            res.append(t_list)
+            s = ' '.join([str(tmp) for tmp in t_list])
+            f.write(s + '\n')
+
+        val = np.array(res)
+        ax = sns.heatmap(val, cbar=True, annot=True)
+        ax.figure.savefig(save_path[:-3] + 'png')
+        # self.tensorboard.log_image('Theta Values', val, epoch)
+        plt.close()
+
+    return res
